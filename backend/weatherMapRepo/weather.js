a66cdb31528b3e22e0e9723813957ebba8256cfe
jlobat93 <lobatojeffrey@gmail.com>
2025-02-15 12:26:21 +0100

    updated url for openweatherapi

--- backend/weatherMapRepo/weather.js
+++ backend/weatherMapRepo/weather.js
@@ -1,8 +1,9 @@
 const axios = require("axios");
 
 const geocode = async function (city) {
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${process.env.API_KEY}`
     try {
-        const { data } = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${process.env.API_KEY}`)
+        const { data } = await axios.get(url)
         // cod is used for the status code, this is based on the error which is returned from the weathermap api
         if (data.length === 0) return { cod: 404, message: 'City not found' }
 
@@ -15,8 +16,9 @@ const geocode = async function (city) {
 }
 
 const getWeather = async function (lat, lon) {
+        const url= `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`
     try {
-        const { data } = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`)
+        const { data } = await axios.get(url)
 
         if (data.length === 0) return { cod: 404, message: 'Weather data not found' }
 
